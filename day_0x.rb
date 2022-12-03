78f015add64cce15808ec15b2ad1db2b9e878524
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2022-12-03 15:24:36 +0100

    improve template

--- day_0x.rb
+++ day_0x.rb
@@ -18,25 +18,25 @@ end
 describe Day0X do
   describe ".part1" do
     it "returns the right value for the test file 1" do
-      input = File.read("inputs/day_01_test_1.txt")
-      expect(Day01.new(input).part1).to eq(nil)
+      input = File.read("inputs/day_0X_test_1.txt")
+      expect(Day0X.new(input).part1).to eq(nil)
     end
 
     it "returns the right value for the test file 2" do
-      input = File.read("inputs/day_01_test_2.txt")
-      expect(Day01.new(input).part1).to eq(nil)
+      input = File.read("inputs/day_0X_test_2.txt")
+      # expect(Day0X.new(input).part1).to eq(nil)
     end
   end
 
   describe ".part2" do
     it "returns the right value for the test file 1" do
-      input = File.read("inputs/day_01_test_1.txt")
-      expect(Day01.new(input).part2).to eq(nil)
+      input = File.read("inputs/day_0X_test_1.txt")
+      # expect(Day0X.new(input).part2).to eq(nil)
     end
 
     it "returns the right value for the test file 2" do
-      input = File.read("inputs/day_01_test_2.txt")
-      expect(Day01.new(input).part2).to eq(nil)
+      input = File.read("inputs/day_0X_test_2.txt")
+      # expect(Day0X.new(input).part2).to eq(nil)
     end
   end
 end
