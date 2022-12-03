e55a2a749b55a8e8a4e69c848742f33345ad816f
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2022-12-03 15:49:26 +0100

    first solution part 2

--- day_03.rb
+++ day_03.rb
@@ -17,24 +17,32 @@ class Day03
     rucksacks
       .map do |rucksack|
         size = rucksack.size
-        first = rucksack[0..size / 2 - 1]
+        first = rucksack[0..size / 2 - 1].split("")
         second = rucksack[size / 2..]
 
         PRIORITIES[
-          first
-            .split("")
-            .select do |char|
-              first.count(char).positive? && second.count(char).positive?
-            end[
-            0
-          ]
+          first.find do |char|
+            first.count(char).positive? && second.count(char).positive?
+          end
         ]
       end
-      .compact
       .sum
   end
 
   def part2
+    rucksacks
+      .each_slice(3)
+      .map do |rucksack1, rucksack2, rucksack3|
+        rucksack1 = rucksack1.split("")
+
+        common =
+          rucksack1.find do |char|
+            rucksack1.count(char).positive? &&
+              rucksack2.count(char).positive? && rucksack3.count(char).positive?
+          end
+        PRIORITIES[common]
+      end
+      .sum
   end
 end
 
@@ -54,12 +62,12 @@ describe Day03 do
   describe ".part2" do
     it "returns the right value for the test file 1" do
       input = File.read("inputs/day_03_test_1.txt")
-      expect(Day03.new(input).part2).to eq(nil)
+      expect(Day03.new(input).part2).to eq(70)
     end
 
     it "returns the right value for the test file 2" do
       input = File.read("inputs/day_03_test_2.txt")
-      # expect(Day03.new(input).part2).to eq(nil)
+      expect(Day03.new(input).part2).to eq(2821)
     end
   end
 end
