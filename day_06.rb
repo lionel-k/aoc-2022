0127c6a0cd68ec139b9aad6be973b047dfef5fdb
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2022-12-06 16:39:00 +0100

    solution day 6

---
+++ day_06.rb
@@ -0,0 +1,68 @@
+require "pry"
+require "rspec/autorun"
+require "set"
+
+# https://adventofcode.com/2022/day/6
+
+# Solution of Day 06 of AOC 2022
+class Day06
+  attr_reader :input
+
+  def initialize(input)
+    @input = input
+  end
+
+  def part1
+    index = 0
+    result = nil
+    while index < input.length - 4 && result.nil?
+      marker = input[index..index + 3]
+      result = marker if Set.new(marker.chars).length == 4
+      index += 1
+    end
+    index + 3
+  end
+
+  def part2
+    index = 0
+    result = nil
+    while index < input.length - 14 && result.nil?
+      marker = input[index..index + 13]
+      result = marker if Set.new(marker.chars).length == 14
+      index += 1
+    end
+    index + 13
+  end
+end
+
+describe Day06 do
+  describe ".part1" do
+    it "returns the right value for the test file 1" do
+      expect(Day06.new("mjqjpqmgbljsphdztnvjfqwrcgsmlb").part1).to eq(7)
+      expect(Day06.new("bvwbjplbgvbhsrlpgdmjqwftvncz").part1).to eq(5)
+      expect(Day06.new("nppdvjthqldpwncqszvftbrmjlhg").part1).to eq(6)
+      expect(Day06.new("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg").part1).to eq(10)
+      expect(Day06.new("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw").part1).to eq(11)
+    end
+
+    it "returns the right value for the test file 2" do
+      input = File.read("inputs/day_06_test_2.txt").chop
+      expect(Day06.new(input).part1).to eq(1598)
+    end
+  end
+
+  describe ".part2" do
+    it "returns the right value for the test file 1" do
+      expect(Day06.new("mjqjpqmgbljsphdztnvjfqwrcgsmlb").part2).to eq(19)
+      expect(Day06.new("bvwbjplbgvbhsrlpgdmjqwftvncz").part2).to eq(23)
+      expect(Day06.new("nppdvjthqldpwncqszvftbrmjlhg").part2).to eq(23)
+      expect(Day06.new("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg").part2).to eq(29)
+      expect(Day06.new("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw").part2).to eq(26)
+    end
+
+    it "returns the right value for the test file 2" do
+      input = File.read("inputs/day_06_test_2.txt").chop
+      expect(Day06.new(input).part2).to eq(2414)
+    end
+  end
+end
